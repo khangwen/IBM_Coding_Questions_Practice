5b28c5a23046427eccafe2ac924b3877ae954c36
Alan Nguyen <a_wen197@yahoo.com>
2023-03-24 01:16:27 -0700

    formatting

--- src/com/ibmcode/solutions/Main.java
+++ src/com/ibmcode/solutions/Main.java
@@ -1,4 +1,14 @@
 package com.ibmcode.solutions;
 
 public class Main {
+    public static void main(String[] args){
+        // Main driver = new Main();
+
+        // Q. _1560
+        _1560.Solution1 sol = new _1560.Solution1();
+        int[] array = new int[]{ 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+        System.out.println(sol.mostVisited(2, array));
+
+
+    }
 }
